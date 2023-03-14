550faba6a99e7081bd863a76cbc69b18942bfe0f
Arnaldo Santos <arnaldo-l-santos@telecom.pt>
2023-03-14 21:16:23 +0000

    Create Head component

---
+++ app/components/Head.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export function _Head() {
+  return (
+    <Head>
+      <title>EAE</title>
+      <meta name="description" content="Fullstack develiper test" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
